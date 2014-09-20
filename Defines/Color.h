45f920a0271ff2955450a2f0973d252798b711b4
ZhangWei <bjxa1101209228@163.com>
2014-09-20 09:30:00 +0800

    new file

---
+++ Defines/Color.h
@@ -0,0 +1,53 @@
+//
+//  Color.h
+//  BANJIA
+//
+//  Created by TeekerZW on 14/9/4.
+//  Copyright (c) 2014年 TEEKER. All rights reserved.
+//
+
+#ifndef BANJIA_Color_h
+#define BANJIA_Color_h
+
+#define USER_NAME_COLOR    [UIColor blackColor]
+
+#define BgViewColor        UIColorFromRGB(0xf0f1f0)
+#define NavigationBgColor  UIColorFromRGB(0xf7f7f7)
+
+#pragma mark - 导航栏
+#define RightCornerTitleColor  UIColorFromRGB(0x898989)
+#define TITLE_COLOR            UIColorFromRGB(0x666464)
+
+#pragma mark - 空间动态
+#define CONTENTCOLOR       [UIColor blackColor]//UIColorFromRGB(0x666666)
+#define TIMECOLOR          UIColorFromRGB(0xcccccc)
+#define LocationColor      UIColorFromRGB(0x999999)
+#define DongTaiNameColor   UIColorFromRGB(0x2f2725)
+#define PraiseColor        UIColorFromRGB(0x727171)
+#define DongTaiBorderColor UIColorFromRGB(0xe1e1e1)
+#define COMMENTCOLOR       UIColorFromRGB(0x666666)
+#define MARK_COLOR          UIColorFromRGB(0x666666)
+
+#pragma mark - 动态评论
+#define DongTaiCommentName   UIColorFromRGB(0x40c46d)
+#define BackButtonTitleColor UIColorFromRGB(0x898989)
+#define CommentLineColor     UIColorFromRGB(0xe3e4e4)
+#define CommentBgColor       UIColorFromRGB(0xfbfbfb)
+#define DongtaiBorderColor   UIColorFromRGB(0xe1e1e1)
+
+#pragma mark - 个人详情
+#define MarkTitleColor    UIColorFromRGB(0x666464)
+#define ContentTextColor  UIColorFromRGB(0x999999)
+
+#pragma mark - 我的班级
+#define MyClassEven  UIColorFromRGB(0x40c46e)
+#define MyClassOdd   UIColorFromRGB(0x41c4b6)
+
+#define LineBackGroudColor  UIColorFromRGB(0xcdcdcd)
+
+#define HEADER_GREEN  UIColorFromRGB(0x3dc46e)
+#define HEADER_BLUER  UIColorFromRGB(0x41c4b6)
+
+#define CHAT_CONTENT_COLOR [UIColor blackColor]
+
+#endif
